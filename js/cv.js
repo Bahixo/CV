312dd979c50c8b91eaa52971f1a2ed8846a05de1
Bahixo <bahixo@hotmail.fr>
2020-05-29 14:43:41 +0200

    correction taille d'affichage

--- js/cv.js
+++ js/cv.js
@@ -1,9 +1,9 @@
 function card(){
     if($("#carte").css("display")==="none"){
         $("#carte").css("display","block");
-        $("#carteDeVisite").css("width","225px");
+        $("#carteDeVisite").css("width","393.75px");
     }else{
         $("#carte").css("display","none");
-        $("#carteDeVisite").css("width","150px");
+        $("#carteDeVisite").css("width","262.5px");
     }
 }
