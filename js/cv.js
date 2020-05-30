e66cb7bf336d07c10247b0e5d87e59fd96dc1f35
Bahixo <bahixo@hotmail.fr>
2020-05-30 11:29:01 +0200

    rajout de l'alert formulaire

--- js/cv.js
+++ js/cv.js
@@ -7,3 +7,7 @@ function card(){
         $("#carteDeVisite").css("width","262.5px");
     }
 }
+
+function alertInfo(){
+    alert("Ce formulaire n'est la qu'à titre démonstratif pour l'instant.nMerci d'avoir pris le temps d'explorer mon CV.nN'hésitez pas à me contacter par mail (Voir carte de visite)")
+}
