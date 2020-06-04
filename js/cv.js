3edcf00dc458f0021dd5f575570ebc6f46ea264b
Bahixo <bahixo@hotmail.fr>
2020-06-04 12:16:51 +0200

    responsive carte de visite vw<=600

--- js/cv.js
+++ js/cv.js
@@ -1,10 +1,25 @@
 function card(){
-    if($("#carte").css("display")==="none"){
-        $("#carte").css("display","block");
-        $("#carteDeVisite").css("width","393.75px");
-    }else{
-        $("#carte").css("display","none");
-        $("#carteDeVisite").css("width","262.5px");
+    if(document.querySelector("body").offsetWidth>600){
+        if($("#carte").css("display")==="none"){
+            $("#carte").css("display","block");
+            $("#carteDeVisite").css("width","393.75px");
+        }else{
+            $("#carte").css("display","none");
+            $("#carteDeVisite").css("width","262.5px");
+        }
+    }
+}
+
+function cardZoom(){
+    if(document.querySelector("body").offsetWidth<600){
+        let cShape=document.getElementById("carte");
+        if(cShape.className!= "cBig"){
+            $("#carte").removeClass();
+            $("#carte").addClass("cBig");
+        }else if(cShape.className==="cBig"){
+            $("#carte").removeClass("cBig");
+            $("#carte").addClass("cLittle");
+        }
     }
 }
 
